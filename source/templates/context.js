93dd1215ef27e17a5230a0e6972d5975f4eb0ffb
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-03-19 07:56:10 -0400

    update styles

--- source/templates/context.js
+++ source/templates/context.js
@@ -4,14 +4,6 @@ export default `
         position: absolute;
         visibility: hidden;
         opacity: 0;
-        background-color: var(--background-color);
-        box-sizing: var(--box-sizing);
-        box-shadow: var(--box-shadow);
-        border: var(--border);
-        border-radius: var(--border-radius);
-        outline: var(--outline);
-        padding: var(--padding);
-        transition: var(--transition);
     }
 </style>
 <slot></slot>
